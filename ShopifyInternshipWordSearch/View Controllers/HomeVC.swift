8097975a3039fa872409a9ebaa563344f1f9c7c7
nickKearns <nickkearns133@gmail.com>
2020-09-04 22:45:39 -0400

    basic 1st view complete

--- ShopifyInternshipWordSearch/View Controllers/HomeVC.swift
+++ ShopifyInternshipWordSearch/View Controllers/HomeVC.swift
@@ -12,14 +12,91 @@ class HomeVC: UIViewController {
     
     
     
+    //create the title label
+    let titleLabel: UILabel = {
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.backgroundColor = .systemGray6
+        l.text = "Word Search"
+        l.font = UIFont(name: "Avenir", size: 25)
+        l.textAlignment = .center
+        return l
+    }()
     
-    let 
+    //create the details label
+    let detailsLabel: UILabel = {
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.backgroundColor = .systemGray6
+        l.text = """
+        Find various words
+        in the grid of letters
+        """
+        l.font = UIFont(name: "Avenir", size: 25)
+        l.textAlignment = .center
+        l.numberOfLines = 0
+        return l
+    }()
+    
+    //create the play button
+    let playButton: UIButton = {
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        b.setTitle("Play!", for: .normal)
+        b.titleLabel?.font = UIFont(name: "Avenir", size: 25)
+        b.backgroundColor = .systemBlue
+        b.layer.cornerRadius = 5
+        b.titleLabel?.textAlignment = .center
+        
+        
+        return b
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemGray6
         // Do any additional setup after loading the view.
+        setupLabels()
+        setupButton()
     }
 
 
+    
+    
+    
+    func setupLabels() {
+        //add titleLabel to the view and constrain it
+        self.view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
+        
+        //add detailsLabel to the view and constrain it
+        self.view.addSubview(detailsLabel)
+        NSLayoutConstraint.activate([
+            detailsLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 100),
+            detailsLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+
+        
+        ])
+
+    }
+    
+    func setupButton() {
+        //add the button to view and constrain it
+        self.view.addSubview(playButton)
+        NSLayoutConstraint.activate([
+            playButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            playButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            playButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.50)
+            
+        
+        
+        ])
+        
+    }
+    
 }
 
